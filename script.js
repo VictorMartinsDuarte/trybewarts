72fa75d52b47b56fed3f738d15dd8b281568e6a5
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-21 17:39:34 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputLogin = document.querySelector('#inputLogin');
 const inputPassword = document.querySelector('#inputPassword');
 const submitButton = document.querySelector('#submitButton');
+const selectHouse = document.querySelector('#house');
 
 submitButton.addEventListener('click', () => {
   if (inputLogin.value !== 'tryber@teste.com' || inputPassword.value !== '123456') {
@@ -9,3 +10,14 @@ submitButton.addEventListener('click', () => {
     alert('Olá, Tryber!');
   }
 });
+
+function houseOptions() {
+  const houses = ['Gitnória', 'Reactpuff', 'Corvinode', 'Pytherina'];
+  const housesId = ['gitnoria-house', 'reactpuff-house', 'corvinode-house', 'pytherina-house'];
+  for (let index = 0; index < houses.length; index += 1) {
+    const houseOption = document.createElement('option');
+    selectHouse.appendChild(houseOption).innerText = houses[index];
+    houseOption.value = houses[index];
+    houseOption.id = housesId[index];
+  }
+} houseOptions();
